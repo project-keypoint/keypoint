a99400f22be22183bfeff433279c9e660490a97b
magpies92 <[github>
2023-10-17 18:21:29 +0900

    강수빈 페이지 부서별 권환 끝

--- src/main/java/com/keypoint/security/AccessFilter.java
+++ src/main/java/com/keypoint/security/AccessFilter.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class AccessFilter implements Filter {
 
@@ -24,38 +23,75 @@ public class AccessFilter implements Filter {
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
 
 		String requestURI = httpRequest.getRequestURI();
-		
-		if (requestURI.equals(httpRequest.getContextPath() + "/main/login") || 
-			    requestURI.startsWith(httpRequest.getContextPath() + "/employee/loginPro") ||
-			    requestURI.startsWith(httpRequest.getContextPath() + "/resources/")) {
+
+		if (requestURI.equals(httpRequest.getContextPath() + "/main/login")
+				|| requestURI.startsWith(httpRequest.getContextPath() + "/employee/loginPro")
+				|| requestURI.startsWith(httpRequest.getContextPath() + "/employee/logout")
+				|| requestURI.startsWith(httpRequest.getContextPath() + "/main/productShipRate")
+				|| requestURI.startsWith(httpRequest.getContextPath() + "/main/monthlyShip")
+				|| requestURI.startsWith(httpRequest.getContextPath() + "/notice/recentNotice")
+				|| requestURI.startsWith(httpRequest.getContextPath() + "/resources/")) {
 			// 로그인 페이지 및 로그인 처리 페이지로의 요청은 항상 허용
 			chain.doFilter(request, response);
 		} else {
 			if (httpRequest.getSession().getAttribute("empId") != null) {
-				// 사용자가 로그인되어 있으면 요청을 허용
-				chain.doFilter(request, response);
-//			} else {
-//				// 사용자가 로그인되어 있지 않으면 경고 메시지를 표시한 후 로그인 페이지로 이동 여부를 확인
-//				String loginPage = httpRequest.getContextPath() + "/main/login";
-//				String alertMessage = "로그인이 필요한 서비스입니다. 로그인 페이지로 이동하시겠습니까?";
-//				String confirmationScript = String.format(
-//						"if(confirm('%s')){window.location.href='%s';} else {window.close();}", alertMessage,
-//						loginPage);
-//				;
-//
-//				httpResponse.setContentType("text/html;charset=UTF-8");
-//				PrintWriter out = httpResponse.getWriter();
-//				out.println("<script>" + confirmationScript + "</script>");
+				if (httpRequest.getSession().getAttribute("empRole").equals(3)) {
+					chain.doFilter(request, response);
+				}
+				else if (httpRequest.getSession().getAttribute("departmentName").equals("인사부")) {
+					if (requestURI.startsWith(httpRequest.getContextPath() + "/employee/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/main/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/employee/employeeDetails")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/notice/")) {
+						chain.doFilter(request, response);
+					}
+				} else if (httpRequest.getSession().getAttribute("departmentName").equals("영업부")) {
+					if (requestURI.startsWith(httpRequest.getContextPath() + "/employee/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/main/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/employee/employeeDetails")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/notice/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/receive/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/customer/")) {
+						chain.doFilter(request, response);
+					}
+				} else if (httpRequest.getSession().getAttribute("departmentName").equals("자재부")) {
+					if (requestURI.startsWith(httpRequest.getContextPath() + "/employee/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/main/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/employee/employeeDetails")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/notice/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/material/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/product/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/purchase/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/receipt/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/require/")) {
+						chain.doFilter(request, response);
+					}
+				} else if (httpRequest.getSession().getAttribute("departmentName").equals("생산부")) {
+					if (requestURI.startsWith(httpRequest.getContextPath() + "/employee/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/main/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/employee/employeeDetails")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/notice/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/workorder/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/line/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/production/")
+							|| requestURI.startsWith(httpRequest.getContextPath() + "/qc/")) {
+						chain.doFilter(request, response);
+					}
+				}
+
+				// 사용자가 로그인되어 있지 않으면 알림창 띄우고 메인 페이지로 리다이렉트
+				String backwardScript = "<script>alert('접근권한이 없습니다.');" + "window.location.href='"
+						+ httpRequest.getContextPath() + "/main/main'</script>";
+				httpResponse.setContentType("text/html;charset=UTF-8");
+				httpResponse.getWriter().write(backwardScript);
+
 			} else {
-                // 사용자가 로그인되어 있지 않으면 알림창 띄우고 로그인 페이지로 리다이렉트
-                String alertScript = "<script>alert('로그인이 필요합니다.'); window.location.href='" +
-                        httpRequest.getContextPath() + "/main/login'</script>";
-                httpResponse.setContentType("text/html;charset=UTF-8");
-                httpResponse.getWriter().write(alertScript);
-                
-                
-            }
-			
+				// 사용자가 로그인되어 있지 않으면 알림창 띄우고 로그인 페이지로 리다이렉트
+				String alertScript = "<script>alert('로그인이 필요합니다.'); " + "window.location.href='"
+						+ httpRequest.getContextPath() + "/main/login'</script>";
+				httpResponse.setContentType("text/html;charset=UTF-8");
+				httpResponse.getWriter().write(alertScript);
+			}
 		}
 	}
 
