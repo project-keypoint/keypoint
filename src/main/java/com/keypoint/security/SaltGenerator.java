d7826db57d8e2a8ecf2adeba8a75be141ba8f32f
magpies92 <[github>
2023-10-18 15:04:50 +0900

    강수빈 솔트값생성기

--- src/main/java/com/keypoint/security/SaltGenerator.java
+++ src/main/java/com/keypoint/security/SaltGenerator.java
@@ -1,14 +1,18 @@
 package com.keypoint.security;
 
-
 import org.apache.commons.codec.binary.Base64;
 import java.security.SecureRandom;
 
 public class SaltGenerator {
-    public static String generateSalt() {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        return Base64.encodeBase64String(salt);
-    }
+	public static String generateSalt() {
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[16];
+		random.nextBytes(salt);
+		System.out.println(Base64.encodeBase64String(salt));
+		return Base64.encodeBase64String(salt);
+	}
+
+	public static String getSalt() {
+		return generateSalt();
+	}
 }
